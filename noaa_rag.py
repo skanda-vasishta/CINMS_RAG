fc5d7762fae73ad244d91e8568cc05f57c1ba88f
Skanda Vasishta <skandavasishta@ucsb.edu>
2024-12-04 02:58:36 -0800

    reverting

--- noaa_rag.py
+++ noaa_rag.py
@@ -31,6 +31,19 @@ load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 OpenAI.api_key = api_key
 
+import streamlit as st
+from streamlit_chromadb_connection.chromadb_connection import ChromadbConnection
+configuration = {
+    "client": "PersistentClient",
+    "path": "/tmp/.chroma"
+}
+collection_name = "documents_collection"
+conn = st.connection("chromadb",
+                     type=ChromaDBConnection,
+                     **configuration)
+documents_collection_df = conn.get_collection_data(collection_name)
+st.dataframe(documents_collection_df)
+
 def load_documents():
     document_loader = PyPDFDirectoryLoader('data')
     return document_loader.load()
