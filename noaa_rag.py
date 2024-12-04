1dd28ba666c0f896cad2e78905a4a6107794f316
Skanda Vasishta <skandavasishta@ucsb.edu>
2024-12-04 03:14:36 -0800

    more chromadb fixes

--- noaa_rag.py
+++ noaa_rag.py
@@ -14,6 +14,12 @@ from langchain.prompts import ChatPromptTemplate
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+from streamlit import logger
+import sqlite3
+
+app_logger = logger.get_logger('SMI_APP')
+app_logger.info(f"sqlite version: {sqlite3.sqlite_version}")
+app_logger.info(f"sys version: {sys.version}")
 
 CHROMA_PATH = 'chroma'
 PROMPT_TEMPLATE = """
